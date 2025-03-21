86ed636b9a9fff54fea813a065e47cc593bd2395
dylan tam <348572314@gapps.yrdsb.ca>
2025-03-21 14:44:44 -0400

    changed stuff

--- Student.java
+++ Student.java
@@ -2,4 +2,5 @@ public class Student
 {
   String firstName = "Man";
   int age = 18;
+  int grade = 11;
 }
---
+++ test.java
@@ -0,0 +1,6 @@
+public class testingGround
+{
+    int num1 = 9;
+    int num2 = 39;
+    System.out.println(num1 += num2);
+}
